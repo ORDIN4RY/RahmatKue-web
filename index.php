202af8665d1a8a38b1e121ac6231f23dcb0223ab
RafyN28 <mrafinaufal28@gmail.com>
2025-09-25 09:08:47 +0700

    beranda-produk-unfix

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rahmat Kue</title>
-     <link rel="icon" type="image/x-icon" href="../RahmatKue-web/assets/icon.png">
+    <link rel="icon" type="image/x-icon" href="../RahmatKue-web/assets/img/icon.png">
+    <link rel="stylesheet" href="../RahmatKue-web/assets/css/main.css">
 </head>
+
 <body>
-    
-</body>
+    </head>
+
+    <body>
+
+        <!-- Navbar -->
+        <div class="navbar">
+            <p>Rahmat Kue.</p>
+            <a href="#" class="active">Beranda</a>
+            <a href="produk.php">Produk</a>
+            <a href="tentang.php">Tentang</a>
+            <a href="#">Login</a>
+        </div>
+
+        <!-- Header -->
+        <section class="header">
+            <div class="header-text">
+                <h1>Rahmat Tahalu asik</h1>
+                <p>
+                    Menyediakan Beranekaragam Kue dengan kualitas wadidaw & Layanan yang sigma86.<br>
+                    <br>Jam Operasional (setiap hari):<br>
+                    06.00 - 21.00
+                </p>
+                <a href="produk.php" class="btn">Jelajahi Produk</a>
+            </div>
+            <div class="icon-header"><img src="./assets/img/logo.png" alt="" srcset=""></div>
+        </section>
+
+    </body>
+
 </html>
---
+++ tentang.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="../RahmatKue-web/assets/img/icon.png">
+    <link rel="stylesheet" href="../RahmatKue-web/assets/css/main.css">
+</head>
+<body>
+    <!-- Navbar -->
+        <div class="navbar">
+            <p>Rahmat Kue.</p>
+            <a href="index.php">Beranda</a>
+            <a href="produk.php">Produk</a>
+            <a href="#" class="active">Tentang</a>
+            <a href="#">Login</a>
+        </div>
+
+        <!-- Header -->
+        <section class="produk-section">
+            <h2>Produk Kami</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus. In eu bibendum orci.</p>
+
+            <div class="produk-container">
+                <!-- Card 1 -->
+                <div class="produk-card">
+                    <img src="./assets/img/Nasgor.png" alt="Kue Coklat">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn">Jelajahi Produk</a>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="produk-card">
+                    <img src="./assets/img/Nasgor.png" alt="Kue Coklat">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn">Jelajahi Produk</a>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="produk-card">
+                    <img src="./assets/img/Nasgor.png" alt="Kue Coklat">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn">Jelajahi Produk</a>
+                </div>
+
+                <!-- Card 4 -->
+                <div class="produk-card">
+                    <img src="./assets/img/Nasgor.png" alt="Kue Coklat">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn">Jelajahi Produk</a>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
