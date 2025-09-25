e36d5f11067bb3eb7ff66910b26f17a60b5e26ac
e41240787 <e41240787@student.polije.ac.id>
2025-09-25 10:53:16 +0700

    nambah beranda

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
         <!-- Navbar -->
         <div class="navbar">
-            <p>Rahmat Kue.</p>
+            <p>Rahmat Kue</p>
             <a href="#" class="active">Beranda</a>
             <a href="produk.php">Produk</a>
             <a href="tentang.php">Tentang</a>
@@ -35,6 +35,43 @@
                 <a href="produk.php" class="btn">Jelajahi Produk</a>
             </div>
             <div class="icon-header"><img src="./assets/img/logo.png" alt="" srcset=""></div>
+        </section>
+
+        <section class="tentang">
+            <div class="icon-tentang"><img src="./assets/img/logo.png" alt="" srcset=""></div>
+            <div class="tentang-text">
+                <h1>Tentang Kami</h1>
+                <p>
+                    dolor sit amet, consectetur adipiscing elit. Vivamus at<br>
+                    sapien velit. Sed ultrices vulputate mi in mollis.
+                    Integer pulvinar quam at tortor facilisis varius a sit <br>
+                    amet dolor. dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+        </section>
+
+        <section class="layanan-section">
+                <h1>Layanan Rahmat Kue</h1>
+                 <div class="layanan-container">
+                <!-- Card 1 -->
+                <div class="layanan-card">
+                    <h3>Layanan<br>Pelanggan</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn-layanan">Hubungi Kami</a>
+                </div>
+                <!-- Card 2 -->
+                <div class="layanan-card">
+                    <h3>Layanan<br>Pelanggan</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn-layanan">Hubungi Kami</a>
+                </div>
+                <!-- Card 3 -->
+                <div class="layanan-card">
+                    <h3>Layanan<br>Pelanggan</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat neque metus.</p>
+                    <a href="#" class="btn-layanan">Hubungi Kami</a>
+                </div>
+            </div>
         </section>
 
     </body>
